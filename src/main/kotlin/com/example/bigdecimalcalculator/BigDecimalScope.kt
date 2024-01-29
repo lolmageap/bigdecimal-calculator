78a32244d8b88011bda520f75eacad91d7387127
cherhy <ekxk1234@naver.com>
2024-01-29 22:12:02 +0900

    create DSL

---
+++ src/main/kotlin/com/example/bigdecimalcalculator/BigDecimalScope.kt
@@ -0,0 +1,48 @@
+package com.example.bigdecimalcalculator
+
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+
+fun bigDecimalScope(
+    scale: Int = 2,
+    roundingMode: RoundingMode = RoundingMode.HALF_EVEN,
+    init: BigDecimalScope.() -> Unit,
+): BigDecimal {
+    val scope = BigDecimalScope(scale, roundingMode)
+    scope.init()
+    return scope.value
+}
+
+class BigDecimalScope(
+    private val scale: Int,
+    private val roundingMode: RoundingMode,
+) {
+
+    var value: BigDecimal = BigDecimal.ZERO
+
+    operator fun Number.plus(other: Number): BigDecimal {
+
+        return this.toBigDecimal().add(other.toBigDecimal())
+    }
+
+    operator fun Number.minus(other: Number): BigDecimal {
+        return this.toBigDecimal().subtract(other.toBigDecimal())
+    }
+
+    operator fun Number.times(other: Number): BigDecimal {
+        return this.toBigDecimal().multiply(other.toBigDecimal())
+    }
+
+    operator fun Number.div(other: Number): BigDecimal {
+        return this.toBigDecimal().divide(other.toBigDecimal(), scale, roundingMode)
+    }
+
+    operator fun Number.rem(other: Number): BigDecimal {
+        return this.toBigDecimal().remainder(other.toBigDecimal())
+    }
+
+}
+
+fun Number.toBigDecimal(): BigDecimal = BigDecimal(this.toString())
