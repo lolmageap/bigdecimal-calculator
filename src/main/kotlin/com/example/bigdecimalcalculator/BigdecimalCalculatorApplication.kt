8007cb1351ab05f17bd1dc18afe7e44e9910991f
cherhy <ekxk1234@naver.com>
2024-01-29 22:11:13 +0900

    create project

---
+++ src/main/kotlin/com/example/bigdecimalcalculator/BigdecimalCalculatorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bigdecimalcalculator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BigdecimalCalculatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<BigdecimalCalculatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bigdecimalcalculator/BigdecimalCalculatorApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.bigdecimalcalculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BigdecimalCalculatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
